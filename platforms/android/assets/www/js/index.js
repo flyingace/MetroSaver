b52b666f9e8bab5ac566db8c8d11e6289b5f72db
flyingace <dc@davidcameron.biz>
2015-03-24 23:06:38 -0400

    Adding latest files for Android build 2.0.3

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -33,18 +33,6 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function () {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function (id) {
-        var parentElement = document.getElementById(id),
-            listeningElement = parentElement.querySelector('.listening'),
-            receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
     }
 };
 
